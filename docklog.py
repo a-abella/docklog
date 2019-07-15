119441eddf708546a8bb16b7d5eed2f3fc5d5182
Antonio Abella <antonio.abella@smartbear.com>
2019-07-15 16:19:03 -0400

    Gross workaround for containers returning bytes vs strings

--- docklog.py
+++ docklog.py
@@ -20,29 +20,55 @@ def maximum_length(nmax):
     class MaximumLength(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
             if not len(values) <= nmax:
-                msg="too many {} arguments: {}. Max 8".format(self.dest,len(values))
+                msg='too many {} arguments: {}. Max 8'.format(self.dest,len(values))
                 raise argparse.ArgumentTypeError(msg)
             setattr(args, self.dest, values)
     return MaximumLength
 
 # Print a docker log stream
 def stream_log(container, color):
-    # Retrieve log lines live as a byte stream
-    # Decode, prepend formatting, and print
+    # Retrieve log lines live as stream
+    # Decode (if needed), prepend formatting, and print
+
+    # Some containers return strings and some return byte streams; apparently
+    # this has to do with container TTY allocation (docker-py issue 1729). We'll
+    # attempt to decode, and if output is char strings then stitch the lines
+    # back together.
     if args.timestamps:
         try:
             tabwidth = (bignamewidth - len(container.name)) + 8
+            strline = ''
             for line in container.logs(stream=True, timestamps=True, tail=args.tail):
-                time = line.decode().strip().split()[0][:22] + "Z"
-                logline = ' '.join(line.decode().strip().split()[1:])
-                print(color + container.name + Style.RESET_ALL + " " * tabwidth + time + "  |  " + logline, flush=True)
+                try:
+                    line = line.decode().strip()
+                    time = line.split()[0][:22] + 'Z'
+                    logline = ' '.join(line.split()[1:])
+                    print(color + container.name + Style.RESET_ALL + ' ' * tabwidth + time + '  |  ' + logline, flush=True)
+                except AttributeError:
+                    if not '\n' in line and not '\r' in line:
+                        strline += line
+                    if '\n' in line:
+                        time = strline.strip().split()[0][:22] + 'Z'
+                        logline = ' '.join(strline.strip().split()[1:])
+                        strline = ''
+                        print(color + container.name + Style.RESET_ALL + ' ' * tabwidth + time + '  |  ' + logline, flush=True)
         except KeyboardInterrupt:
             return 1
     else:
         try:
             tabwidth = (bignamewidth - len(container.name)) + 14
+            strline = ''
             for line in container.logs(stream=True, timestamps=False, tail=args.tail):
-                print(color + container.name + Style.RESET_ALL + " " * tabwidth  + "|  " + line.decode().strip(), flush=True)
+                try:
+                    logline = line.decode().strip()
+                    print(color + container.name + Style.RESET_ALL + ' ' * tabwidth  + '|  ' + logline, flush=True)
+                except AttributeError:
+                    if not '\n' in line and not '\r' in line:
+                        strline += line
+                    if '\n' in line:
+                        logline = strline.strip()
+                        strline = ''
+                        print(color + container.name + Style.RESET_ALL + ' ' * tabwidth  + '|  ' + logline, flush=True)
         except KeyboardInterrupt:
             return 1
 
@@ -54,35 +80,35 @@ def print_log(container, color):
     if args.timestamps:
         try:
             tabwidth = (bignamewidth - len(container.name)) + 8
-            for line in container.logs(stream=False, timestamps=True, tail=args.tail).decode().split("\n")[:-1]:
+            for line in container.logs(stream=False, timestamps=True, tail=args.tail).decode().split('\n')[:-1]:
                 logline = line.strip()
-                time = logline.split()[0][:24] + "Z"
+                time = logline.split()[0][:24] + 'Z'
                 logline = ' '.join(logline.split()[1:])
-                thislog.append(color + container.name + Style.RESET_ALL + " " * tabwidth  + time + "  |  " + logline)
+                thislog.append(color + container.name + Style.RESET_ALL + ' ' * tabwidth  + time + '  |  ' + logline)
             return thislog
         except KeyboardInterrupt:
             return 1
     else:
         try:
             tabwidth = (bignamewidth - len(container.name)) + 14
-            for line in container.logs(stream=False, timestamps=False, tail=args.tail).decode().split("\n")[:-1]:
-                thislog.append(color + container.name + Style.RESET_ALL + " " * tabwidth  + "|  " + line.strip())
+            for line in container.logs(stream=False, timestamps=False, tail=args.tail).decode().split('\n')[:-1]:
+                thislog.append(color + container.name + Style.RESET_ALL + ' ' * tabwidth  + '|  ' + line.strip())
             return thislog
         except KeyboardInterrupt:
             return 1
 
 # Parse arguments and options
 # Print error if too many containers
-parser = argparse.ArgumentParser(description="Simultaneously stream the logs of up to eight Docker containers")
-parser.add_argument("container", metavar="CONTAINER", help="Container names or IDs", type=str, nargs="+", action=maximum_length(8))
-parser.add_argument("-t", "--timestamps", "--time", help="Prepend timestamps to log lines", action="store_true")
-parser.add_argument("-n", "--tail", help="Number of lines to show from end (default 10)", type=int, default=10)
-parser.add_argument("-s", "--static", help="Do not follow; print tail lines and exit", action="store_true")
+parser = argparse.ArgumentParser(description='Simultaneously stream the logs of up to eight Docker containers')
+parser.add_argument('container', metavar='CONTAINER', help='Container names or IDs', type=str, nargs='+', action=maximum_length(8))
+parser.add_argument('-t', '--timestamps', '--time', help='Prepend timestamps to log lines', action='store_true')
+parser.add_argument('-n', '--tail', help='Number of lines to show from end (default 10)', type=int, default=10)
+parser.add_argument('-s', '--static', help='Do not follow; print tail lines and exit', action='store_true')
 try:
     args = parser.parse_args()
 except argparse.ArgumentTypeError as err:
     parser.print_usage()
-    print(os.path.basename(parser.prog) + ": " + "error: " + str(err))
+    print(os.path.basename(parser.prog) + ': ' + 'error: ' + str(err))
     sys.exit(1)
 
 # Hold used colors and init ANSI sequences in win32
@@ -106,13 +132,13 @@ for container in args.container:
     try:
         client = docker.from_env(version='auto', assert_hostname=False)
     except:
-        print("\n" + Style.BRIGHT + "\033[31mError" + Style.RESET_ALL + ": Could not connect to docker daemon")
+        print('\n' + Style.BRIGHT + '\033[31mError' + Style.RESET_ALL + ': Could not connect to docker daemon')
         deinit()
         sys.exit(1)
     try:
         thiscontainer = client.containers.get(container)
     except:
-        print("\n" + Style.BRIGHT + "\033[31mError" + Style.RESET_ALL + ": Could not find container '" + container + "'")
+        print('\n' + Style.BRIGHT + '\033[31mError' + Style.RESET_ALL + ': Could not find container '' + container + ''')
         client.close()
         deinit()
         sys.exit(1)
@@ -134,7 +160,7 @@ for container in args.container:
         if not colorcode in usedcolors:
             usedcolors.append(colorcode)
             break
-    color = Style.BRIGHT + "\033[" + str(colorcode) + "m"
+    color = Style.BRIGHT + '\033[' + str(colorcode) + 'm'
 
     # Connect to docker using DOCKER_HOST env var
     client = docker.from_env(version='auto', assert_hostname=False)
